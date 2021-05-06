8ed0bda5af99d60a3447870dcdc5d2f86ec60052
Luka Lickah <lukalickah@MacBook-Air-de-Luka.local>
2021-05-06 20:33:03 -0300

    vini/06

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!-- FOOTER -->
+<footer class="text-white bg-dark text-center text-lg-start">
+    
+    <div class="container p-4">
+
+        <div class="row">
+            
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+
+                <form action="">
+
+                    <div class="row d-flex justify-content-center">
+
+                        <div class="col-10">
+                            <p class="pt-2">
+                            <strong>Registre-se para receber novidades no email</strong>
+                            </p>
+                        </div>
+                        
+                        <div class="col-md-5 col-12">
+                            <div class="form-outline form-white mb-4">
+                                <input type="email" class="form-control" />
+                                <label class="form-label" for="form5Example2">Email de contato</label>
+                            </div>
+                        </div>
+
+                        <div class="col-2">
+                            <button type="submit" class="btn btn-success">Enviar</button>
+                        </div>
+
+                    </div>
+
+                </form>
+
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase"><b>REDES SOCIAIS</b></h5>
+
+                <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#" class="text-white">Twitter</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white">Instagram</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white">Facebook</a>
+                </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0 ">
+                <h5 class="text-uppercase"><b>SOBRE NÓS</b></h5>
+
+                <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#" class="text-white">Comunidade</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white">História</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white">Parceiros</a>
+                </li>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <p class="m-0 text-center text-white">2021 - IFMS || Rafael Socovoski - Vinicius Moura - Vinicius Buchara - Luka Lickah - Victor Casemiro - Gabriel Moraleco</p>
+  </div>
+
+</footer>
